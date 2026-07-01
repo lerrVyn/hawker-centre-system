0f471c4da5321c72179bc54f3303da7aefa25d94
HuiWeiYe <S10275211@connect.np.edu.sg>
2026-07-02 00:02:14 +0800

    Added Placeholder files and directories

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const sql = require('mssql');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dbConfig = require("./dbConfig");
+const app = express();
+
+
+
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+
+
+// Start server
+app.listen(PORT, async () => {
+  try {
+    // Connect to the database
+    await sql.connect(dbConfig);
+    console.log("Database connection established successfully");
+  } catch (err) {
+    console.error("Database connection error:", err);
+    process.exit(1);
+  }
+
+  console.log(`Server listening on port ${PORT}`);
+});
