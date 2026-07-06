4ef94c4395b58be518e8ec2797772445f140fcb5
Lervyn Ang <lervynnn@gmail.com>
2026-07-06 13:34:30 +0800

    adjustment to code

--- dbConfig.js
+++ dbConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   trustServerCertificate: true,
   options: {
     port: parseInt(process.env.DB_PORT), // Default SQL Server port
